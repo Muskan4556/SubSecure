3c6e3226516f2fe262a8aece94fe9c6699431569
Muskan4556 <muskan35163@gmail.com>
2026-02-20 20:11:27 +0530

    frontend: initial config

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-3xl text-blue-600 m-4 font-bold">
+      <div>Hello World</div>
+      <Button>Submit</Button>
+    </div>
+  );
+}
